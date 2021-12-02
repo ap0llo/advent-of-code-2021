341216c3189d2aa53f044974c9de9f5070dd7233
Andreas Gruenwald <andreas@grynwald.net>
2021-12-02 21:42:47 +0100

    Advent of Code 2021, Days 1 and 2

---
+++ 01/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+var input = File.ReadAllLines("input.txt")
+    .Where(x => !String.IsNullOrEmpty(x))
+    .Select(int.Parse)
+    .ToArray();
+
+int GetPart1()
+{
+
+    int numberOfIncreases = 0;
+    int? previousValue = null;
+    foreach (var value in input)
+    {
+        if (previousValue is null)
+        {
+            Console.WriteLine("(N/A - no previous measurement)");
+        }
+        else if (previousValue.Value == value)
+        {
+            Console.WriteLine("(No Change)");
+        }
+        else if (previousValue.Value < value)
+        {
+            Console.WriteLine("(increased)");
+            numberOfIncreases += 1;
+        }
+        else
+        {
+            Console.WriteLine("(decreased)");
+        }
+        previousValue = value;
+    }
+
+    return numberOfIncreases;
+}
+
+int GetPart2()
+{
+    int numberOfIncreases = 0;
+    int? previousSum = null; 
+
+    for(int i = 0; i < input.Length - 2; i++)
+    {
+        var currentSum = input[i] + input[i + 1] + input[i + 2];
+
+        if (previousSum is not null && currentSum > previousSum.Value)
+            numberOfIncreases += 1;
+
+        previousSum = currentSum;
+    }
+
+
+    return numberOfIncreases;
+
+}
+
+
+var part1 = GetPart1();
+var part2 = GetPart2();
+Console.WriteLine($"Answer 1: {part1}");
+Console.WriteLine($"Answer 2: {part2}");
---
+++ 02/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+var input = File.ReadAllLines("input.txt")
+    .Where(x => !String.IsNullOrEmpty(x))
+    .ToArray();
+
+int GetPart1()
+{
+    int depth = 0;
+    int horizontalPosition = 0;
+  
+
+    foreach(var line in input)
+    {
+        var split = line.Split(' ');
+        var command = split[0];
+        var arg = int.Parse(split[1]);
+
+
+        switch (command)
+        {
+            case "forward":
+                horizontalPosition += arg;
+                break;
+
+            case "down":
+                depth += arg;
+                break;
+
+            case "up":
+                depth -= arg;
+                break;
+
+            default:
+                throw new NotImplementedException();
+        }
+
+    }
+
+    return depth * horizontalPosition;
+}
+
+int GetPart2()
+{
+    int depth = 0;
+    int horizontalPosition = 0;
+    int aim = 0;
+
+    foreach (var line in input)
+    {
+        var split = line.Split(' ');
+        var command = split[0];
+        var x = int.Parse(split[1]);
+
+        switch (command)
+        {
+            case "forward":
+                horizontalPosition += x;
+                depth += (aim * x);
+                break;
+
+            case "down":
+                aim += x;
+                break;
+
+            case "up":
+                aim -= x;
+                break;
+
+            default:
+                throw new NotImplementedException();
+        }
+
+    }
+
+    return depth * horizontalPosition;
+}
+
+
+var part1 = GetPart1();
+Console.WriteLine($"Answer 1: {part1}");
+var part2 = GetPart2();
+Console.WriteLine($"Answer 2: {part2}");
